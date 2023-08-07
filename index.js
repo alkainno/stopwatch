d8b65f9f7637b62983a73bb3a2c511414b68cf53
alkainno <alka.chauhan@innotechteam@gmail.com>
2023-08-07 13:26:02 +0530

    completed stopwatch

--- index.js
+++ index.js
@@ -1,79 +1,97 @@
-let startBtn = document.getElementById("start");
-let stopBtn = document.getElementById("stop");
+let startStopBtn = document.getElementById("startStop");
 let resetBtn = document.getElementById("reset");
 let lapBtn = document.getElementById("lap");
+let lapList = document.getElementById("lapList");
+let display = document.getElementById("display");
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
+let startTime = 0;
+let timerInterval = null;
+let elapsed = 0;
+let laps = [];
+let previousLapTime = 0;
 
-startBtn.addEventListener("click", function () {
-  timer = true;
-  stopWatch();
-});
-
-stopBtn.addEventListener("click", function () {
-  timer = false;
+startStopBtn.addEventListener("click", function () {
+  if (timerInterval === null) {
+    startTimer();
+    startStopBtn.textContent = "Stop";
+    lapBtn.style.display = "inline-block";
+  } else {
+    stopTimer();
+    startStopBtn.textContent = "Start";
+  }
 });
 
 resetBtn.addEventListener("click", function () {
-  timer = false;
-  hour = 0;
-  minute = 0;
-  second = 0;
-  count = 0;
-  document.getElementById("hr").innerHTML = "00";
-  document.getElementById("min").innerHTML = "00";
-  document.getElementById("sec").innerHTML = "00";
-  document.getElementById("count").innerHTML = "00";
+  stopTimer();
+  display.textContent = "00:00:00:00";
+  laps = [];
+  lapList.innerHTML = "";
+  startStopBtn.textContent = "Start";
+  elapsed = 0; // Reset the elapsed time to 0 when the timer is reset
+  previousLapTime = 0; // Reset the previous lap time
 });
 
-function stopWatch() {
-  if (timer) {
-    count++;
-
-    if (count == 100) {
-      second++;
-      count = 0;
+lapBtn.addEventListener("click", function () {
+  if (timerInterval !== null) {
+    laps.push(getElapsedTime());
+    let lapTime = formatTime(laps[laps.length - 1]);
+    let lapItem = document.createElement("li");
+    if (laps.length === 1) {
+      lapItem.textContent = lapTime;
+      let lapHeading = document.createElement("h4");
+      lapHeading.textContent = "Laps";
+      lapList.appendChild(lapHeading);
+    } else {
+      let lapDifference = laps[laps.length - 1] - previousLapTime;
+      let lapDifferenceTime = formatTime(lapDifference);
+      lapItem.textContent = lapDifferenceTime;
     }
-
-    if (second == 60) {
-      minute++;
-      second = 0;
-    }
-
-    if (minute == 60) {
-      hour++;
-      minute = 0;
-      second = 0;
-    }
-
-    let hrString = hour;
-    let minString = minute;
-    let secString = second;
-    let countString = count;
-
-    if (hour < 10) {
-      hrString = "0" + hrString;
-    }
-
-    if (minute < 10) {
-      minString = "0" + minString;
-    }
-
-    if (second < 10) {
-      secString = "0" + secString;
-    }
-
-    if (count < 10) {
-      countString = "0" + countString;
-    }
-
-    document.getElementById("hr").innerHTML = hrString;
-    document.getElementById("min").innerHTML = minString;
-    document.getElementById("sec").innerHTML = secString;
-    document.getElementById("count").innerHTML = countString;
-    setTimeout(stopWatch, 10);
+    lapList.appendChild(lapItem);
+    previousLapTime = laps[laps.length - 1];
   }
+});
+
+function startTimer() {
+  if (elapsed === 0) {
+    startTime = Date.now() - (laps.length > 0 ? laps[laps.length - 1] : 0);
+  } else {
+    startTime = Date.now() - elapsed; // Use the stored elapsed time as the start time
+  }
+  timerInterval = setInterval(updateTimer, 10);
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+  elapsed = getElapsedTime(); // Store the elapsed time when the timer is stopped
+}
+
+function updateTimer() {
+  let elapsedTime = getElapsedTime();
+  display.textContent = formatTime(elapsedTime);
+}
+
+function getElapsedTime() {
+  return Date.now() - startTime;
+}
+
+function formatTime(time) {
+  let hours = Math.floor(time / 3600000);
+  let minutes = Math.floor((time % 3600000) / 60000);
+  let seconds = Math.floor((time % 60000) / 1000);
+  let centiseconds = Math.floor((time % 1000) / 10);
+
+  return (
+    padNumber(hours) +
+    ":" +
+    padNumber(minutes) +
+    ":" +
+    padNumber(seconds) +
+    ":" +
+    padNumber(centiseconds)
+  );
+}
+
+function padNumber(num) {
+  return num.toString().padStart(2, "0");
 }
